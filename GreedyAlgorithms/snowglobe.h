ea8178ccc32eaf99cd8fa1915e1de53f63f57816
fireflamemm <jmm15f@acu.edu>
2019-01-25 19:19:36 -0600

    Added snowglobe struct

---
+++ GreedyAlgorithms/snowglobe.h
@@ -0,0 +1,29 @@
+#include <cmath>
+using namespace std;
+
+struct snowglobe
+{
+  string id;
+  int weight;
+  int value;
+  double ratio;
+  snowglobe(i,w,v) : id(i), weight(w), value(v)
+  {
+    ratio = value/weight;
+  }
+};
+
+int sortWeight()
+{
+
+}
+
+int sortValue()
+{
+
+}
+
+int sortRatio()
+{
+
+}
