ac6e0aaf4b40197095aadf8023f0d30273b8bfab
Bethany Witemeyer <brw15a@acu.edu>
2019-01-26 21:58:32 -0600

    file read in

--- GreedyAlgorithms/knapsack.cpp
+++ GreedyAlgorithms/knapsack.cpp
@@ -10,10 +10,17 @@ using namespace std;
 int main()
 {
   //create the vector to hold all the snowglobes (and the other variables)
-  vector<snowglobe> house;
+  vector<snowglobe*> house;
   string filename;
   string line;
   ifstream fin;
+
+  int numItems = 0;
+  int maxWeight = 0;
+
+  string snowglobeID;
+  int snowglobeWeight = 0;
+  int snowglobeValue = 0;
 
   //get the input file
   cout << "Enter filename: ";
@@ -26,10 +33,33 @@ int main()
   {
     //the first line has num items and max weight, so do that separately
     getline(fin, line);
-    cout << "before: " << line << endl;
+
     //replace the commas with spaces
     replace(line.begin(), line.end(), ',', ' ');
-    cout << "after: " << line << endl;
+
+    //now we get the two numbers
+    stringstream ss(line);
+    ss >> numItems >> maxWeight;
+
+    //loop through the rest of the file to get the snowglobes
+    for (int i = 0; i < numItems; i++)
+    {
+      //read in the line and replace the commas
+      getline(fin, line);
+      replace(line.begin(), line.end(), ',', ' ');
+      stringstream ssSnowglobe(line);
+
+      //get the individual components and put them into a struct
+      ssSnowglobe >> snowglobeID >> snowglobeWeight >> snowglobeValue;
+      snowglobe* current = new snowglobe;
+      current->id = snowglobeID;
+      current->weight = snowglobeWeight;
+      current->value = snowglobeValue;
+      current->ratio = (double)(snowglobeValue) / snowglobeWeight;
+
+      //put the snowglobe into the house
+      house.push_back(current);
+    }
   }
   else
   {
@@ -37,4 +67,12 @@ int main()
   }
 
   fin.close();
+
+  //the sorting stuff will go here
+
+  //this is the delete loop for all the pointer we are now done with
+  for (int i = 0; i < house.size(); i++)
+  {
+    delete house[i];
+  }
 }
