1ef87149cf2dc48e7772d48713dbd0755affc4df
fireflamemm <jmm15f@acu.edu>
2019-02-04 20:03:06 -0600

    Created batch files to run all MarWit Datasets and examine a specific file.

---
+++ runAll.bat
@@ -0,0 +1,38 @@
+echo Run Datasets with exe
+echo.
+echo -----------------------------------MarWitData1---------------------------
+echo.
+echo DataSets\MarWitData1.kp | exhaust.exe
+echo.
+echo DataSets\MarWitData1.kp | greed.exe
+echo.
+echo DataSets\MarWitData1.kp | prune.exe
+echo.
+
+echo -----------------------------------MarWitData2---------------------------
+echo.
+echo DataSets\MarWitData2.kp | exhaust.exe
+echo.
+echo DataSets\MarWitData2.kp | greed.exe
+echo.
+echo DataSets\MarWitData2.kp | prune.exe
+echo.
+
+echo -----------------------------------MarWitData3---------------------------
+echo.
+echo DataSets\MarWitData3.kp | exhaust.exe
+echo.
+echo DataSets\MarWitData3.kp | greed.exe
+echo.
+echo DataSets\MarWitData3.kp | prune.exe
+echo.
+
+echo -----------------------------------MarWitData4---------------------------
+echo.
+echo DataSets\MarWitData4.kp | greed.exe
+echo.
+
+echo -----------------------------------MarWitData5---------------------------
+echo.
+echo DataSets\MarWitData5.kp | greed.exe
+echo.
---
+++ runFile.bat
@@ -0,0 +1,11 @@
+ECHO OFF
+ECHO Run %1 with all executables
+ECHO.
+ECHO -----------------------------------%1---------------------------
+ECHO.
+ECHO %1 | exhaust.exe
+ECHO.
+ECHO %1 | greed.exe
+ECHO.
+ECHO %1 | prune.exe
+ECHO.
